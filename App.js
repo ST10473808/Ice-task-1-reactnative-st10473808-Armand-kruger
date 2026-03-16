8e6614364e4e9f2d7aceb150300584715eb67ce6
ST10473808@vcconnect.edu.za <ST10473808@vcconnect.edu.za>
2026-03-16 14:07:33 +0200

    Initial commit of my Maroon app

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Change 'blue' to your actual favourite color! */}
+      <Text style={styles.myText}>My favourite color is maroon</Text>
     </View>
   );
 }
@@ -16,5 +16,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  myText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    
+    color: '#800000', 
   },
 });
